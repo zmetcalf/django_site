11418e077d41a12cff9f9046bd56af333c5a78e2
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-05-21 17:21:46 -0600

    Initial Commit

---
+++ polls/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+class Choic(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    

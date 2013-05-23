f5c46de1407e7c3d01d3b9cd9c76e86ec3d0c1bd
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-05-22 20:59:37 -0600

    Finished first part of tutorial

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,23 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Poll(models.Model):
+
+    def __unicode__(self):
+        return self.question
+        
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     
-class Choic(models.Model):
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+    
+class Choice(models.Model):
+
+    def __unicode__(self):
+        return self.choice_text
+        
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)

949517e7504020ebfea293805e777e784374a774
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-05-29 13:49:12 -0600

    Finished Part 3, requires 1.5+ Django

--- django_site/urls.py
+++ django_site/urls.py
@@ -13,6 +13,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace = "polls")),
     url(r'^admin/', include(admin.site.urls)),
 )

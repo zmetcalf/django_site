b2f2056752456ead82bcdf12a0a4ef3d08184804
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-05-29 13:24:58 -0600

    Long version of 404 errors

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from polls import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name = 'index'),
-    
+   
     url(r'^(?P<poll_id>\d+)/$', views.detail, name = 'detail'),
     
     url(r'^(?P<poll_id>\d+)/results/$', views.results, name = 'results'),
--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,19 @@
-from django.http import HttpResponse
-from django.template import Context, loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 from polls.models import Poll
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = Context({
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk = poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, "polls/detail.html", {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)

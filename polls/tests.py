7a8770b03dd116fb81cfb1c8ec19993885177523
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-05-30 12:36:50 -0600

    Added tests

--- polls/tests.py
+++ polls/tests.py
@@ -4,13 +4,23 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
 
+from django.utils import timezone
 from django.test import TestCase
 
+from polls.models import Poll
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PollMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_poll(self):
+        future_poll = Poll(pub_date = timezone.now() + datetime.timedelta(days = 30))
+        self.assertEqual(future_poll.was_published_recently(), False)
+        
+    def test_was_published_recently_with_old_poll(self):
+        old_poll = Poll(pub_date = timezone.now() - datetime.timedelta(days = 30))
+        self.assertEqual(old_poll.was_published_recently(), False)
+        
+    def test_was_published_recently_with_recent_poll(self):
+        recent_poll = Poll(pub_date = timezone.now() - datetime.timedelta(hours = 1))
+        self.assertEqual(recent_poll.was_published_recently(), True)
